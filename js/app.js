cc0e3b90d3e3a5cf4993f8f8fb2b8fa8d6a2e686
malik albeik <abd.beik@me.com>
2018-01-03 19:35:43 +0300

    Added responsiven design for mobiles and other devices

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 var deck = document.querySelector('.deck');
 var currentlySelectedCards = []; /* An array containing the currently played two cards */
 var shuffledCards = shuffle(Array.from(document.getElementsByClassName('card')));
@@ -10,21 +8,25 @@ var numberOfTimesTheUserWasWrong = 0;
 /**
  * Setting the time and adding it to the .timer claased node.
  */
-var seconds = 0, minutes = 0, hours = 0, startTimer;
+var seconds = 0,
+  minutes = 0,
+  hours = 0,
+  timer;
+
 function add() {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
+  seconds++;
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
     }
-    document.querySelector('.timer').textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-    startTimer = setTimeout(add, 1000);
+  }
+  document.querySelector('.timer').textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+  timer = setTimeout(add, 1000);
 }
-startTimer = setTimeout(add, 1000);
+timer = setTimeout(add, 1000);
 
 
 
@@ -35,17 +37,17 @@ startTimer = setTimeout(add, 1000);
  */
 deck.innerHTML = "";
 for (var i = 0; i < shuffledCards.length; i++) {
-    deck.appendChild(shuffledCards[i]);
+  deck.appendChild(shuffledCards[i]);
 }
 
 
 /**
  * event listener for when the deck is clicked.
  */
-deck.addEventListener('click', function(event){
+deck.addEventListener('click', function(event) {
 
   /* An if statment to check if the clicked element is a deck or a card. */
-  if (event.target.firstElementChild.classList[1] !== undefined && event.target.classList.contains('open','show') === false){
+  if (event.target.firstElementChild.classList[1] !== undefined && event.target.classList.contains('open', 'show') === false) {
     currentlySelectedCards.push(event.target.firstElementChild.classList[1]);
   }
   event.target.classList.add('show', 'open');
@@ -54,23 +56,23 @@ deck.addEventListener('click', function(event){
   var cardsWithOpenClass = [].slice.call(document.querySelectorAll('.open')).filter(e => (e.className.indexOf('match') === -1));
 
   /*
-  * check if the two cards match each other or not,
-  * if so match them if not then close them.
-  */
+   * check if the two cards match each other or not,
+   * if so match them if not then close them.
+   */
   if (currentlySelectedCards[0] === currentlySelectedCards[1]) {
-     cardsWithOpenClass.forEach(function(element) {
-      element.classList.add('match');
-     })
-  }else if (currentlySelectedCards[1]) {
     cardsWithOpenClass.forEach(function(element) {
-      setTimeout(function () {
+      element.classList.add('match');
+    })
+  } else if (currentlySelectedCards[1]) {
+    cardsWithOpenClass.forEach(function(element) {
+      setTimeout(function() {
         element.classList.remove('open', 'show');
       }, 250);
     });
     numberOfTimesTheUserWasWrong += 1;
   }
 
-  if(currentlySelectedCards.length === 2) currentlySelectedCards = [];
+  if (currentlySelectedCards.length === 2) currentlySelectedCards = [];
 
   document.querySelector('.moves').innerHTML = 9 - numberOfTimesTheUserWasWrong;
 
@@ -91,44 +93,48 @@ deck.addEventListener('click', function(event){
       var lostModal = document.querySelector('.lost-modal');
       lostModal.style.display = "block";
       document.querySelector('.timer-lost').textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-      clearTimeout(t);
+      clearTimeout(timer);
       break;
     default:
       break;
-  };
+  }
 
 
   /* check for when the player has won the game and present the win modal.*/
-  if (Array.from(document.querySelectorAll('.match')).length >= 16){
+  if (Array.from(document.querySelectorAll('.match')).length >= 16) {
     var winModal = document.querySelector('.win-modal');
     var closeButton = document.querySelector('.win-modal-close');
     document.querySelector('.timer-won').textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-    clearTimeout(t);
-    clearTimeout(t);
+    clearTimeout(timer);
     winModal.style.display = "block";
+    document.querySelector('.players-rating').appendChild(stars);
 
+    /* to close the win modal when the user presses on the window or the close button */
     closeButton.onclick = function() {
-    winModal.style.display = "none";
-    };
+      winModal.style.display = "none";
+    }
     window.onclick = function(event) {
       if (event.target == winModal) {
-          winModal.style.display = "none";
+        winModal.style.display = "none";
       }
     }
   }
 
-});
-
-document.querySelector('.restart').addEventListener('click', function(){
-  document.querySelectorAll('.card').forEach(function(element) {
-    element.classList.remove('match', 'open', 'show');
-    location.reload();
-  });
-
-});
+})
 
 
 
+/**
+ * setting the reset button.
+ */
+document.querySelectorAll('.restart').forEach(function(element) {
+  element.addEventListener('click', function() {
+    document.querySelectorAll('.card').forEach(function(el) {
+      el.classList.remove('match', 'open', 'show');
+      location.reload();
+    })
+  })
+})
 
 
 /*
@@ -140,17 +146,18 @@ document.querySelector('.restart').addEventListener('click', function(){
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 /*
